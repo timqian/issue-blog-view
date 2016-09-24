32868339f32e5709a429c24e1ff3c35e4a8310ff
timqian <timqian92@qq.com>
2016-09-24 11:11:29 +0800

    init

--- issueList.js
+++ issueList.js
@@ -1,5 +1,11 @@
-var el = document.querySelector('.container')
+var dq = document.querySelector.bind(document)
+var dqA = document.querySelectorAll.bind(document)
 
-el.remove()
+var removeThem = ['div.header', 'div.container nav']
+removeThem.forEach(item => {
+  dq(item).style.display = 'none'
+})
 
-console.log('333')
+dqA('div.float-left.pt-3.pl-3').forEach(el => {
+  el.style.display = 'none'
+})
--- issueView.js
+++ issueView.js
@@ -1,5 +1,12 @@
-var el = document.querySelector('.container')
+var dq = document.querySelector.bind(document)
 
-el.remove()
+var removeThem = ['div.header', 'div.container nav', 'span.gh-header-number',
+  '#partial-discussion-sidebar > div', '.sidebar-milestone', '.sidebar-assignee']
+removeThem.forEach(item => {
+  dq(item).style.display = 'none'
+})
 
-console.log('222')
+dq('div.flex-table').innerHTML = ''
+
+dq('textarea').style['max-height'] = '1000px'
+dq('textarea').style['min-height'] = '500px'
